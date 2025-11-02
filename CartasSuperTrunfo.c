bbb8a4d6485c47914041a3a016f4b0b0313a12af
Paulo7kprogramador <paulo.7ala@gmail.com>
2025-11-02 11:59:42 +0000

    cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,9 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-    char estado;
-    char codigo[10];
-    char cidade[20];
+    char estado[30];
+    char codigo[30];
+    char cidade[50];
     int populacao;
     float areaemkm²;
     float pib;
@@ -36,12 +36,44 @@ int main() {
     printf("Digite o Numero de Pontos Turisticos \n");
     scanf("%d", &pontosturisticos);
 
-    printf("Nome/estado: %s\n", estado);
+    printf("Estado: %s\n", estado);
     printf("Codigo: %d\n", codigo);
-    printf("Nome/Cidade: %s\n",cidade);
+    printf("Cidade: %s\n",cidade);
     printf("Populção: %d\n", populacao);
-    printf("Area em km²: %f\n", areaemkm²);
-    printf("Pib: %f\n", pib);
+    printf("Area em km²: %.2f\n", areaemkm²);
+    printf("Pib: %.2f\n", pib);
+    printf("Pontos Turisticos: %d\n", pontosturisticos);
+    printf("\n");
+    
+    printf("Desafio Super Trunfo 1!\n");
+
+    printf("Digite o Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo: \n");
+    scanf("%d", &codigo);
+
+    printf("Digite a Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite a População: \n");
+    scanf("%f", &populacao);
+
+    printf("Digite a Area em Km²: \n");
+    scanf("%f", &areaemkm²);
+
+    printf("Digite o Pib: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o Numero de pontos Turisticos: \n");
+    scanf("%d", &pontosturisticos);
+
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %d\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Area em Km²: %.2f\n", areaemkm²);
+    printf("Pib: %.2f\n", pib);
     printf("Pontos Turisticos: %d\n", pontosturisticos);
 
     return 0;
